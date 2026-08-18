c5e6f68372bde74e012af2b36f448c9e5e33c8b4
R4G3RUNN3R <ragerunner__gr@hotmail.com>
2026-08-18 15:07:06 +0100

    Restore production security header while initializing knowledge corpus

--- server/index.ts
+++ server/index.ts
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   app.use((_req, res, next) => {
     res.setHeader("X-Content-Type-Options", "nosniff");
     res.setHeader("X-Frame-Options", "SAMEORIGIN");
-    res.setHeader("X-XSS-Protection", "0");
+    res.setHeader("X-XSS-Protection", "1; mode=block");
     res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
     next();
   });
